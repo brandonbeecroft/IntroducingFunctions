6eaecd2b3466e2cd8438b6a8dd32841bf9c0ea4b
Brandon Beecroft <brandonbeecroft@gmail.com>
2014-08-26 22:03:39 -0600

    complete step 4

--- IntroducingFuctions-Objc/IntroducingFuctions-Objc/AppDelegate.m
+++ IntroducingFuctions-Objc/IntroducingFuctions-Objc/AppDelegate.m
@@ -20,6 +20,9 @@
     // Insert code here...
     NSString *myName = @"Brandon";
 
+    //step 4
+    [self countDownTillIntroduction:4];
+
     // the instructions to do this were very unclear. I did not see anywhere the concept of declaring self in the recommended reading.
     [self printIntroductions: myName];
     
@@ -36,7 +39,17 @@
 
     NSLog(@"%@", introduction);
     NSLog(@"%@", japaneseIntroduction); // Not sure this is the output desired?
+}
 
+-(void)countDownTillIntroduction:(int)numberOfDays {
+    if (numberOfDays == 0) {
+        NSLog(@"The time has come");
+    }
+    else if (numberOfDays != 0) {
+        NSLog(@"%i, days left until introductions", numberOfDays);
+        int oneLessDay = numberOfDays - 1;
+        [self countDownTillIntroduction:oneLessDay];
+    }
 }
 
 @end
